62367f9bf781a44a74e5d038b4f592d655f18537
olster2115 <ot210@kent.ac.uk>
2026-02-15 00:24:05 +0000

    def add members PT1

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,12 +5,12 @@ def main():
         D = ["Command","Command" ,"Medical" ,"Engineering","Engineering"]
         ID = ["1","2" ,"3" ,"4" ,"5"]
         
-        def init_database ():
+        def init_database (N:list[str],R:list[str],D:list[str],ID:list[str]):
                 print(N,R, D ,ID)
-                return 1
+                return 
                         
         init_database()
-        def display_menu():
+        def display_menu(N:list[str],R:list[str],D:list[str],ID:list[str]):
                 print("Add Members:")
                 print("Remove Members:")
                 print("Update Members:")
@@ -23,5 +23,28 @@ def main():
                 return 
         display_menu()                            
         
+        def add_members(N:list[str],R:list[str],D:list[str],ID:list[str]):
+                print("add member")
+                N = input("")
+                print("add rank")
+                R = input("")
+                print("add division")
+                D= input("")
+                print("Add ID")
+                ID = input("")
+                if ID in ID:
+                        print("ID already in use try again")
+                        return
+                if R not in R:
+                        print("rank does not exist")
+                        return
+                N.append(input(""))
+                R.append(input(""))
+                D.append(input(""))
+                ID.append(input("")) 
+                
+                return
+        add_members()
        
+
 main()
